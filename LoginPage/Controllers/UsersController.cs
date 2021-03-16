958841237520b8b452e756563ed7e3d933623df2
PythonForLearn <53744848+PythonForLearn@users.noreply.github.com>
2021-03-16 20:46:47 +0700

    Update UsersController.cs

--- LoginPage/Controllers/UsersController.cs
+++ LoginPage/Controllers/UsersController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Cors;
 namespace LoginPage.Controllers
 {
     [Route("api/[controller]")]
@@ -14,6 +14,7 @@ namespace LoginPage.Controllers
     {
         [HttpGet]
         [Authorize]
+        [EnableCors("CorsPolicy")]
         public IEnumerable<string> Get()
             => new string[] { "ty123", "123456" };
     }
